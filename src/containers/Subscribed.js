f47688d19e0783e72f73142e1c45ac927b48c64d
Andrew Jun <97andrewjun@gmail.com>
2021-01-11 13:45:47 -0800

    Add authentication to subscribe/unsubscribe

--- src/containers/Subscribed.js
+++ src/containers/Subscribed.js
@@ -3,6 +3,8 @@ import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { APP_INFO } from '../constants';
 import '../components/Auth/AuthModal.css';
 import Banner from '../components/UI/Banner';
+import { useKeycloak } from '@react-keycloak/web';
+import { Redirect } from 'react-router-dom';
 
 export const Subscribed = () => {
   const { keycloak } = useKeycloak();
