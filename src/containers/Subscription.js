873eddc09a9670063cc644654c61010a17702fa1
Andrew Jun <97andrewjun@gmail.com>
2021-01-11 14:01:26 -0800

    add parameters to infrastructure code

--- src/containers/Subscription.js
+++ src/containers/Subscription.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { APP_INFO } from '../constants';
 import '../components/Auth/AuthModal.css';
