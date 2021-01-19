fe347dd54764c3b65d2e6c23bb7b43499f1d2215
Andrew Jun <97andrewjun@gmail.com>
2021-01-19 14:08:12 -0800

    reformat to redirect after check

--- src/containers/Subscription.js
+++ src/containers/Subscription.js
@@ -24,47 +24,46 @@ import { Redirect } from 'react-router-dom';
 export const Subscription = () => {
   const { keycloak } = useKeycloak();
   console.log(keycloak);
-  // Back to root route if the user is not authenticated
-  if (keycloak.authenticated === false){
-    console.log('redirect')
-    return (
-      <Redirect to="/" />
-    )
-  }else if (keycloak.authenticated === true) {
+
+  if (keycloak.authenticated === true) {
     const userEmail = keycloak.idTokenParsed.email; 
+    return (
+      <Modal modalClassName="auth-modal" isOpen={true} fade={false}>
+        <ModalHeader>
+          <Banner titleText={APP_INFO.NAME} />
+        </ModalHeader>
+        <ModalBody>
+          Subscribe/unsubscribe from the mautic mailing list.
+          <br />
+          Your email address is <b>{userEmail}</b>.
+        </ModalBody>
+        <ModalFooter>
+          <div className="auth-buttons">
+            {/* <button className="auth-button" onclick={subscription("subscribe")}>Subscribe</button> 
+            <button className="auth-button" onclick={subscription("unsubscribe")}>Unsubscribe</button>  */}
+            <form action="http://mautic-de0974-tools.apps.silver.devops.gov.bc.ca/form/submit?formId=1" method="post">
+              <input className="auth-button" type="submit" value="Subscribe"/>
+              <input type="hidden" name="mauticform[emailsubscribe]" value={userEmail} ></input>
+              <input type="hidden" name="mauticform[formId]" id="mauticform_subscribe_id" value="1"></input>
+              <input type="hidden" name="mauticform[return]" id="mauticform_subscribe_return" value=""></input>
+              <input type="hidden" name="mauticform[formName]" id="mauticform_subscribe_name" value="subscribe"></input>
+            </form>
+            <form action="http://mautic-de0974-tools.apps.silver.devops.gov.bc.ca/form/submit?formId=2" method="post">
+              <input className="auth-button" type="submit" value="Unsubscribe"/>
+              <input type="hidden" name="mauticform[emailunsubscribe]" value={userEmail}></input>
+              <input type="hidden" name="mauticform[formId]" id="mauticform_unsubscribe_id" value="2"></input>
+              <input type="hidden" name="mauticform[return]" id="mauticform_unsubscribe_return" value=""></input>
+              <input type="hidden" name="mauticform[formName]" id="mauticform_unsubscribe_name" value="unsubscribe"></input>
+            </form>
+          </div>
+        </ModalFooter>
+      </Modal>
+    );
   }
+  
   return (
-    <Modal modalClassName="auth-modal" isOpen={true} fade={false}>
-      <ModalHeader>
-        <Banner titleText={APP_INFO.NAME} />
-      </ModalHeader>
-      <ModalBody>
-        Subscribe/unsubscribe from the mautic mailing list.
-        <br />
-        {/* Your email address is <b>{userEmail}</b>. */}
-      </ModalBody>
-      <ModalFooter>
-        <div className="auth-buttons">
-          {/* <button className="auth-button" onclick={subscription("subscribe")}>Subscribe</button> 
-          <button className="auth-button" onclick={subscription("unsubscribe")}>Unsubscribe</button>  */}
-          <form action="http://mautic-de0974-tools.apps.silver.devops.gov.bc.ca/form/submit?formId=1" method="post">
-            <input className="auth-button" type="submit" value="Subscribe"/>
-            {/* <input type="hidden" name="mauticform[emailsubscribe]" value={userEmail} ></input> */}
-            <input type="hidden" name="mauticform[formId]" id="mauticform_subscribe_id" value="1"></input>
-            <input type="hidden" name="mauticform[return]" id="mauticform_subscribe_return" value=""></input>
-            <input type="hidden" name="mauticform[formName]" id="mauticform_subscribe_name" value="subscribe"></input>
-          </form>
-          <form action="http://mautic-de0974-tools.apps.silver.devops.gov.bc.ca/form/submit?formId=2" method="post">
-            <input className="auth-button" type="submit" value="Unsubscribe"/>
-            {/* <input type="hidden" name="mauticform[emailunsubscribe]" value={userEmail}></input> */}
-            <input type="hidden" name="mauticform[formId]" id="mauticform_unsubscribe_id" value="2"></input>
-            <input type="hidden" name="mauticform[return]" id="mauticform_unsubscribe_return" value=""></input>
-            <input type="hidden" name="mauticform[formName]" id="mauticform_unsubscribe_name" value="unsubscribe"></input>
-          </form>
-        </div>
-      </ModalFooter>
-    </Modal>
-  );
+    <Redirect to="/" />
+  )
 };
 
 
