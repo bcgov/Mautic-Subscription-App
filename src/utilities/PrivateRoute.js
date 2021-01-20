38d86f73f6f48aff540a880f1b0d9d2108646519
Andrew Jun <97andrewjun@gmail.com>
2021-01-20 09:55:49 -0800

    Add PrivateRoute

---
+++ src/utilities/PrivateRoute.js
@@ -0,0 +1,30 @@
+
+import { useKeycloak } from '@react-keycloak/web';
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+
+
+export const PrivateRoute= ({ component: Component, ...rest }) => {
+    const { keycloak } = useKeycloak();
+
+
+    const isAuthorized = () => {
+        if (keycloak?.authenticated) {
+            return keycloak.authenticated;
+        }
+        return false;
+    }
+
+    return (
+        <Route
+            {...rest}
+            render={props => {
+                return isAuthorized()
+                    ? <Component {...props} />
+                    : <Redirect to={{ pathname: '/', }} />
+            }}
+        />
+    )
+}
+
+export default PrivateRoute;
