8536c7870906eb493f260740b6cb48218375e63f
Andrew Jun <97andrewjun@gmail.com>
2021-03-05 13:18:19 -0800

    add role validation

--- src/utilities/PrivateRoute.js
+++ src/utilities/PrivateRoute.js
@@ -7,11 +7,19 @@ import { Redirect, Route } from 'react-router-dom';
 export const PrivateRoute= ({ component: Component, ...rest }) => {
     const { keycloak, initialized } = useKeycloak();
 
+    const isMember = ( keycloak ) => {
+      const userRoles = keycloak.realmAccess.roles  
+      const roles = ["github-org-bcgov", "github-org-bcgov-c", "github-org-bcdevops", "idir-user"];
+      const result = userRoles.filter(role => roles.indexOf(role) > -1);
+      if (result.length > 0) return true
+      return false
+    }
+
     return initialized && (
         <Route
             {...rest}
             render={props => {
-                return keycloak.authenticated
+                return keycloak.authenticated && isMember(keycloak)
                     ? <Component {...props} />
                     : <Redirect to={{ pathname: '/401', }} />
             }}
