7bd66855ea5b28248a5b159132ac4a0b3c1bd1e6
Andrew Jun <97andrewjun@gmail.com>
2021-03-05 14:52:38 -0800

    Update isMember

--- src/utilities/PrivateRoute.js
+++ src/utilities/PrivateRoute.js
@@ -7,19 +7,17 @@ import { Redirect, Route } from 'react-router-dom';
 export const PrivateRoute= ({ component: Component, ...rest }) => {
     const { keycloak, initialized } = useKeycloak();
 
-    const isMember = ( keycloak ) => {
-      const userRoles = keycloak.realmAccess.roles  
+    const isMember = ( keycloak, userRoles) => {
       const roles = ["github-org-bcgov", "github-org-bcgov-c", "github-org-bcdevops", "idir-user"];
       const result = userRoles.filter(role => roles.indexOf(role) > -1);
-      if (result.length > 0) return true
-      return false
+      return result.length > 0
     }
 
     return initialized && (
         <Route
             {...rest}
             render={props => {
-                return keycloak.authenticated && isMember(keycloak)
+                return keycloak.authenticated && isMember(keycloak.realmAccess.roles)
                     ? <Component {...props} />
                     : <Redirect to={{ pathname: '/401', }} />
             }}
