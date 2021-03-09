6d304277257fdc97edb26928ec4d5f259666dc9a
Andrew Jun <97andrewjun@gmail.com>
2021-03-09 15:51:05 -0800

    update authroizedRoles

--- src/utilities/PrivateRoute.js
+++ src/utilities/PrivateRoute.js
@@ -8,16 +8,20 @@ export const PrivateRoute = ({ component: Component, ...rest }) => {
     const { keycloak, initialized } = useKeycloak();
     const authorizedRolesJSON = useConfig('/config/authorizedRoles.json');
 
-    const isMember = ( userRoles, authorizedRoles) => {
+    const isMember = ( userRoles, authorizedRolesJSON) => {
       /**
        * @param {string[]} userRoles user's roles in the realm
-       * @param {string} authorizedRoles comma separated values of authorized roles
+       * @param {string} authorizedRolesJSON JSON object containing comma separated values of authorized roles
        *  @returns {boolean} 
        */
-      const splitAuthorizedRoles = authorizedRoles.split(',');
+
+      // Case where configmap for authorized roles does not exist. Assume everyone is authorized
+      if (authorizedRolesJSON === null) return true
+
+      const splitAuthorizedRoles = authorizedRolesJSON.authorizedRoles.split(',');
 
       // return true if no authorized roles are defined, since everyone would be authorized   
-      if (authorizedRoles.length === 1 && splitAuthorizedRoles[0] === '') return true
+      if (splitAuthorizedRoles.length === 1 && splitAuthorizedRoles[0] === '') return true
       
       // check if the user is authorized
       const result = userRoles.filter(role => splitAuthorizedRoles.indexOf(role) > -1);
@@ -28,12 +32,12 @@ export const PrivateRoute = ({ component: Component, ...rest }) => {
         <Route
             {...rest}
             render={props => {
-                return keycloak.authenticated && isMember(keycloak.realmAccess.roles, authorizedRolesJSON.authorizedRoles)
+                return keycloak.authenticated && isMember(keycloak.realmAccess.roles, authorizedRolesJSON)
                     ? <Component {...props} />
                     : <Redirect to={{ pathname: '/401', }} />
             }}
         />
-    )
+    )   
 }
 
 export default PrivateRoute;    
