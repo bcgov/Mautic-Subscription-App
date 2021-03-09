3876a1dd0c2928108729f21ece6f08e91cb302f6
Andrew Jun <97andrewjun@gmail.com>
2021-03-09 11:25:18 -0800

    add commenting

--- src/utilities/PrivateRoute.js
+++ src/utilities/PrivateRoute.js
@@ -9,8 +9,17 @@ export const PrivateRoute = ({ component: Component, ...rest }) => {
     const authorizedRolesJSON = useConfig('/config/authorizedRoles.json');
 
     const isMember = ( userRoles, authorizedRoles) => {
+      /**
+       * @param {string[]} userRoles user's roles in the realm
+       * @param {string} authorizedRoles comma separated values of roles that are authorized
+       *  @returns {boolean} 
+       */
       const splitAuthorizedRoles = authorizedRoles.split(',');
-      if (authorizedRoles.length===1 && splitAuthorizedRoles[0]==='') return true
+
+      // return true if there are no defined authorized roles since everyone is authorized   
+      if (authorizedRoles.length === 1 && splitAuthorizedRoles[0] === '') return true
+      
+      // check if the user is authorized
       const result = userRoles.filter(role => splitAuthorizedRoles.indexOf(role) > -1);
       return result.length > 0
     }
